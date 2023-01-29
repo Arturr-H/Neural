0e4fccae1f603042505e9bce4607ce19e8484bcf
Artur <artur.h.0427@gmail.com>
2023-01-29 11:31:25 +0100

    Create more activation functions

--- src/activations.rs
+++ src/activations.rs
@@ -1,9 +1,18 @@
 /* Imports */
 use crate::layer::GlobalNNFloatType;
-
 pub type Activation = fn(GlobalNNFloatType) -> GlobalNNFloatType;
 
 /// Step
 pub fn step(input: GlobalNNFloatType) -> GlobalNNFloatType {
     if input > 0. { 1. } else { 0. }
+}
+
+/// Sigmoid
+pub fn sigmoid(input: GlobalNNFloatType) -> GlobalNNFloatType {
+    1. / (1. + (-input).exp())
+}
+
+/// SiLU
+pub fn silu(input: GlobalNNFloatType) -> GlobalNNFloatType {
+    input / (1. + (-input).exp())
 }
