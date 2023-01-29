ea68e116c8fd1555bbb928e017ec0c007a267393
Artur <artur.h.0427@gmail.com>
2023-01-29 11:25:46 +0100

    Fix activation bug

--- src/activations.rs
+++ src/activations.rs
@@ -1,6 +1,9 @@
+/* Imports */
+use crate::layer::GlobalNNFloatType;
+
 pub type Activation = fn(GlobalNNFloatType) -> GlobalNNFloatType;
 
 /// Step
 pub fn step(input: GlobalNNFloatType) -> GlobalNNFloatType {
-    if input > 0 { 1 } else { 0 }
+    if input > 0. { 1. } else { 0. }
 }
