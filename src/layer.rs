c5ea2cde0a8a20f6faed00d876855f2699c926d8
Artur <artur.h.0427@gmail.com>
2023-02-04 21:52:54 +0100

    Add fields `cost_gradient_weights` and `cost_gradient_biases` to `Layer`

--- src/layer.rs
+++ src/layer.rs
@@ -29,7 +29,19 @@ pub struct Layer {
     biases: Vec<GlobalNNFloatType>,
 
     /// The activation function represented by this layer
-    activation: Activation
+    activation: Activation,
+
+    /// The cost gradient weights are a 2d matrix, responding to
+    /// each and every weight in the `weights` matrix. The value
+    /// is determined by how much the cost changes in response to
+    /// a change in the weight.
+    cost_gradient_weights: Vec<Vec<GlobalNNFloatType>>,
+
+    /// The cost gradient biases is an array responding to
+    /// each and every bias in the `biases` array. The value
+    /// is determined by how much the cost changes in response to
+    /// a change in the bias.
+    cost_gradient_biases: Vec<GlobalNNFloatType>,
 }
 
 /* Method implementations */
@@ -39,8 +51,15 @@ impl Layer {
         Self {
             num_nodes_in,
             num_nodes_out,
-            weights: vec![vec![0.;num_nodes_out];num_nodes_in],
-            biases: vec![0.;num_nodes_out],
+
+            /* Weights */
+            weights:               vec![vec![0.;num_nodes_out];num_nodes_in],
+            cost_gradient_weights: vec![vec![0.;num_nodes_out];num_nodes_in],
+
+            /* Biases */
+            biases:               vec![0.;num_nodes_out],
+            cost_gradient_biases: vec![0.;num_nodes_out],
+
             activation: activations::step
         }
     }
@@ -81,10 +100,16 @@ impl Layer {
         err * err
     }
 
-    /* GETTERS */
+    /* [GETTERS] General */
     pub fn num_nodes_in(&self) -> usize { self.num_nodes_in }
     pub fn num_nodes_out(&self) -> usize { self.num_nodes_out }
+    pub fn activation(&self) -> Activation { self.activation }
+    
+    /* [GETTERS] Weights and biases */
     pub fn weights(&self) -> &Vec<Vec<GlobalNNFloatType>> { &self.weights }
     pub fn biases(&self) -> &Vec<GlobalNNFloatType> { &self.biases }
-    pub fn activation(&self) -> Activation { self.activation }
+
+    /* [GETTERS] Cost gradient */
+    pub fn cost_gradient_biases(&self) -> &Vec<GlobalNNFloatType> { &self.cost_gradient_biases }
+    pub fn cost_gradient_weights(&self) -> &Vec<Vec<GlobalNNFloatType>> { &self.cost_gradient_weights }
 }
