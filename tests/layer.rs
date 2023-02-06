1700f148c41814c25ef7b3fbae3b942f2e730788
Artur <artur.h.0427@gmail.com>
2023-02-06 16:21:04 +0100

    Change `tests/layer.rs` to match new layer constructor

--- tests/layer.rs
+++ tests/layer.rs
@@ -11,6 +11,6 @@ mod tests {
         assert_eq!(layer.num_nodes_out() == 10, true);
         assert_eq!(layer.num_nodes_in() == 5, true);
         assert_eq!(layer.biases().len() == 10, true);
-        assert_eq!(layer.weights().len() == 5, true);
+        assert_eq!(layer.weights().len() == 5 * 10, true);
     }
 }
