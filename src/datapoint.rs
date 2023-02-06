b50b32e8967cab77a324a5e50085a22036eb4a0d
Artur <artur.h.0427@gmail.com>
2023-02-06 16:14:25 +0100

    Add `load_mnist` method for `Datapoint`

--- src/datapoint.rs
+++ src/datapoint.rs
@@ -1,5 +1,6 @@
 /* Imports */
 use crate::layer::GlobalNNFloatType;
+use rust_mnist::Mnist;
 
 /* Main */
 pub struct Datapoint {
@@ -12,7 +13,32 @@ pub struct Datapoint {
 
 /* Method implementations */
 impl Datapoint {
+    /// Loads the MNIST dataset
+    pub fn load_mnist<'a>() -> Vec<Datapoint> {
+        let mnist = Mnist::new("./data/");
+        let mut labels = Vec::new();
+
+        for (label, data) in mnist.train_labels.iter().zip(mnist.train_data.iter()) {
+            labels.push(Datapoint {
+                inputs: data.to_vec().iter().map(|e| *e as f64).collect::<Vec<f64>>(),
+                expected: to_byte_array_u8(*label).to_vec().iter().map(|e| *e as f64).collect::<Vec<f64>>(),
+            })
+        };
+
+        labels
+    }
+
     /* Getters */
     pub fn inputs(&self) -> &Vec<GlobalNNFloatType> { &self.inputs }
     pub fn expected(&self) -> &Vec<GlobalNNFloatType> { &self.expected }
+}
+
+fn to_byte_array_u8(value: u8) -> [u8; 8] {
+    let mut array = [0; 8];
+
+    for i in 0..8 {
+        array[7 - i] = (value >> i) & 1;
+    }
+
+    array
 }
