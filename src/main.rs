c102a2ca9283099c2a41b7d6393930f61d4b8262
Artur <artur.h.0427@gmail.com>
2023-02-06 16:13:28 +0100

    Add testing and re-export module

--- src/main.rs
+++ src/main.rs
@@ -5,9 +5,12 @@
     unused_variables
 )]
 
+use crate::datapoint::Datapoint;
+
 /* Modules */
 pub mod layer;
 pub mod network;
+pub mod learndata;
 pub mod datapoint;
 pub mod activations;
 
@@ -15,5 +18,27 @@ pub mod activations;
 
 /* Main */
 fn main() {
-    network::Network::new(&[1, 2, 3, 4]);
+    let mut nn = network::Network::new(&[784, 6, 8, 6, 2]);
+    let training_data = vec![
+        datapoint::Datapoint { inputs: vec![0., 0., 0., 0.], expected: vec![0., 0.] },
+
+        datapoint::Datapoint { inputs: vec![1., 0., 0., 0.], expected: vec![1., 0.] },
+        datapoint::Datapoint { inputs: vec![1., 1., 0., 0.], expected: vec![1., 0.] },
+        datapoint::Datapoint { inputs: vec![0., 1., 0., 0.], expected: vec![1., 0.] },
+
+        datapoint::Datapoint { inputs: vec![0., 0., 1., 0.], expected: vec![0., 1.] },
+        datapoint::Datapoint { inputs: vec![0., 0., 1., 1.], expected: vec![0., 1.] },
+        datapoint::Datapoint { inputs: vec![0., 0., 0., 1.], expected: vec![0., 1.] },
+
+        datapoint::Datapoint { inputs: vec![0., 1., 1., 1.], expected: vec![1., 1.] },
+        datapoint::Datapoint { inputs: vec![1., 1., 1., 0.], expected: vec![1., 1.] },
+        datapoint::Datapoint { inputs: vec![1., 1., 0., 1.], expected: vec![1., 1.] }
+    ];
+    println!("{}", nn);
+
+    let training_data = Datapoint::load_mnist();
+    for i in 0..1000 {
+        nn.learn(&training_data, 0.0001);
+        dbg!(nn.cost());
+    }
 }
